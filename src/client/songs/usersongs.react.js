e87a9e19bfb513ae72995909737ced36e8a329c3
Daniel Steigerwald <daniel@steigerwald.cz>
2015-08-14 20:00:17 +0200

    Add user songs.

---
+++ src/client/songs/usersongs.react.js
@@ -0,0 +1,44 @@
+// import './list.styl';
+import Component from '../components/component.react';
+import Loading from '../components/loading.react';
+import React from 'react';
+import SongLink from '../songs/songlink.react';
+import listenFirebase from '../firebase/listenfirebase';
+import {Link} from 'react-router';
+
+@listenFirebase((props, firebase) => ({
+  action: props.actions.songs.onSongsCreatedByUser,
+  ref: firebase
+    .child('songs')
+    .orderByChild('createdBy')
+    .equalTo(props.viewer.id)
+}))
+export default class UserSongs extends Component {
+
+  static propTypes = {
+    actions: React.PropTypes.object.isRequired,
+    msg: React.PropTypes.object.isRequired,
+    userSongs: React.PropTypes.object.isRequired,
+    viewer: React.PropTypes.object.isRequired
+  }
+
+  render() {
+    const {msg, userSongs, viewer} = this.props;
+    const songs = userSongs.get(viewer.id);
+
+    if (!songs) return <Loading msg={msg} />
+
+    return (
+      <div className="songs-usersongs">
+        <ol>
+          {songs.map(song =>
+            <li key={song.id}>
+              <SongLink song={song} />
+            </li>
+          )}
+        </ol>
+      </div>
+    );
+  }
+
+}
