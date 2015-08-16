a58d8a938bf8f0241b453330f1b3ac02863095ef
Daniel Steigerwald <daniel@steigerwald.cz>
2015-08-16 23:09:34 +0200

    Chore.

---
+++ src/client/songs/add.react.js
@@ -0,0 +1,31 @@
+import Component from '../components/component.react';
+import DocumentTitle from 'react-document-title';
+import React from 'react';
+import SongForm from './songform.react';
+import requireAuth from '../auth/requireauth.react';
+
+@requireAuth
+export default class Add extends Component {
+
+  static propTypes = {
+    actions: React.PropTypes.object.isRequired,
+    msg: React.PropTypes.object.isRequired,
+    songs: React.PropTypes.object.isRequired
+  }
+
+  render() {
+    const {
+      actions: {songs: actions}, msg,
+      songs: {add: song}
+    } = this.props;
+
+    return (
+      <DocumentTitle title={msg.songs.add.title}>
+        <div className="songs-add">
+          <SongForm {...{actions, msg, song}} />
+        </div>
+      </DocumentTitle>
+    );
+  }
+
+}
